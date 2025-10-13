0ad2572bba20f7034fdfa4fdd124df876e351e46
jolaaa <manuel.gjolaj@gmail.com>
2025-10-13 16:48:02 +0200

    done the migration of the project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,21 +6,23 @@ import {AuthModule} from "./auth/auth.module";
 import {AuthController} from "./auth/auth.controller";
 import {ApplicationsModule} from "./apply/applications.module";
 import {TypeOrmModule} from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import {ConfigModule} from '@nestjs/config';
 import {UserSeederModule} from "./auth/user-seeder.module";
 
 @Module({
-    imports: [LocationsModule, AuthModule, ApplicationsModule, ConfigModule.forRoot({isGlobal:true}),
-    TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: process.env.DB_HOST || 'localhost',
-        port: parseInt(process.env.DB_PORT ?? '5434', 10),
-        username: process.env.DB_USER || 'postgres',
-        password: process.env.DB_PASSWORD || 'password',
-        database: process.env.DB_NAME || 'mydb',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true, // solo per sviluppo!
-    }), UserSeederModule
+    imports: [LocationsModule, AuthModule, ApplicationsModule, ConfigModule.forRoot({isGlobal: true}),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            port: parseInt(process.env.DB_PORT ?? '5434', 10),
+            username: process.env.DB_USER || 'postgres',
+            password: process.env.DB_PASSWORD || 'password',
+            database: process.env.DB_NAME || 'mydb',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: false,
+            migrations: [__dirname + '/migrations/*.ts'],
+            migrationsRun: false,
+        }), UserSeederModule
     ],
     controllers: [AppController],
     providers: [AppService],
