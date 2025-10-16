d1cbd2111aaba1c025f9f70c7eccadd4e5c1a560
jolaaa <manuel.gjolaj@gmail.com>
2025-10-16 11:46:37 +0200

    populated the table of locations with import locations

--- backend/src/locations/locations.module.ts
+++ backend/src/locations/locations.module.ts
@@ -1,10 +1,15 @@
 import {Module} from '@nestjs/common';
 import {LocationsController} from './locations.controller';
 import {LocationsService} from './locations.service';
+import { Location } from './locations.entity';
+import {TypeOrmModule} from "@nestjs/typeorm";
+
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Location])],
     controllers: [LocationsController],
     providers: [LocationsService],
+    exports: [TypeOrmModule],
 })
 export class LocationsModule {
 }
