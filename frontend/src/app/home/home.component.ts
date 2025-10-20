e51006a0f2f433fd68d4ef5b9d512cd44fe30ec0
jolaaa <manuel.gjolaj@gmail.com>
2025-10-20 16:01:14 +0200

    installed the bootstrap package and inserted icons and containers for the responsive

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -13,31 +13,41 @@ import {FormsModule} from "@angular/forms";
     imports: [CommonModule, HousingLocationComponent, HttpClientModule, FormsModule],
     providers: [HousingService],
     template: `
-        <header class="header">
-            <form class="search-form" (ngSubmit)="filterResults(filter.value)">
-                <input type="text" placeholder="Filter by city" #filter>
-                <button class="primary" type="submit">Search</button>
-            </form>
+        <div class='container-fluid'>
+            <header class="header">
+                <form class="search-form" (ngSubmit)="filterResults(filter.value)">
+                    <input type="text" placeholder="Filter by city" #filter>
+                    <button class="primary" type="submit">Search</button>
+                </form>
 
-            <div class="user-menu" (click)="toggleMenu()">
-                <span class="user-icon">👤</span>
-                <div *ngIf="menuOpen" class="menu-dropdown">
-                    <button (click)="goToProfile()">Profile</button>
+                <div class="user-menu" (click)="toggleMenu()">
+    <span class="user-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle"
+             viewBox="0 0 16 16">
+          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+          <path fill-rule="evenodd"
+                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+        </svg>
+    </span>
+                    <div *ngIf="menuOpen" class="menu-dropdown">
+                        <button (click)="goToProfile()">Profile</button>
+                    </div>
                 </div>
-            </div>
-        </header>
 
-        <section>
-            <section class="results"></section>
-            <app-housing-location
-                    *ngFor="let housingLocation of filteredLocationList"
-                    [housingLocation]="housingLocation">
-            </app-housing-location>
-        </section>
+            </header>
 
-        <footer class="add-house-footer">
-            <button class="primary add-house-btn" (click)="goToAddHouse()">Nuova Casa</button>
-        </footer>
+            <section>
+                <section class="results"></section>
+                <app-housing-location
+                        *ngFor="let housingLocation of filteredLocationList"
+                        [housingLocation]="housingLocation">
+                </app-housing-location>
+            </section>
+
+            <footer class="add-house-footer">
+                <button class="primary add-house-btn" (click)="goToAddHouse()">Nuova Casa</button>
+            </footer>
+        </div>
     `,
     styleUrls: ['./home.component.css'] //style css
 })
--- frontend/src/app/profile.ts
+++ frontend/src/app/profile.ts
@@ -9,7 +9,15 @@ import {AuthService} from "./AuthService";
     imports: [CommonModule],
     template: `
         <section class="profile">
-            <h2>👤 User Profile</h2>
+            <div class="d-flex align-items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
+                     class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                    <path fill-rule="evenodd"
+                          d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                </svg>
+                <span class="fs-4 fw-semibold">Profile Page</span>
+            </div>
             <p><strong>Email:</strong> {{ email }}</p>
             <button class="logout-btn" (click)="logout()">Logout</button>
         </section>
